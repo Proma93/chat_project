2a6ae3912933c64de6150d3c1c76e441f6c1c971
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-30 00:24:30 +0200

    Set up Channels routing with WebSocket support and authentication

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
+django.setup()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+import chat.routing  # <-- Import after setup
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
