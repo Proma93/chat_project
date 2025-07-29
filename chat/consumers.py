8f798a3f1baea1c3855655a82e39c881776872df
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-30 00:25:13 +0200

    Implement AsyncWebsocketConsumer for real-time chat messaging

---
+++ chat/consumers.py
@@ -0,0 +1,49 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Room, Message
+from django.contrib.auth.models import User
+from channels.db import database_sync_to_async
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        self.user = self.scope["user"]
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+
+        await self.save_message(self.room_name, self.user, message)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'user': event['user']
+        }))
+
+    @database_sync_to_async
+    def save_message(self, room_name, user, message):
+        room = Room.objects.get(name=room_name)
+        Message.objects.create(room=room, user=user, content=message)
