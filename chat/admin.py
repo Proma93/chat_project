785f75c3195a06af14c7edd6c26cb961a0541a7f
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-30 00:20:33 +0200

    Register Room and Message models in admin site

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# chat/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Room, Message
+
+admin.site.register(Room)
+admin.site.register(Message)
